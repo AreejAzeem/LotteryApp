5a7c9f512ad49364e3df8afbb2f715af61e71bde
AreejAzeem <areejazeem67@gmail.com>
2024-07-17 10:25:19 +0500

    set up project architecture

---
+++ lottery_app/pages/index.js
@@ -0,0 +1,18 @@
+
+import Head from 'next/head';
+
+export default function Home() {
+
+  return (
+    <div>
+      <Head>
+        <title>Lottery App</title>
+        <meta name="description" content="Lottery application" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h2>Latest Results</h2>
+      </main>
+    </div>
+  );
+}
